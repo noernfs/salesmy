802bf29e73201dbc29335436c50070f659d2a802
Muhammad Aji <noer.nfs@gmail.com>
2023-08-29 05:23:11 +0700

    fix pwa

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -155,7 +155,7 @@ const Header = () => {
                   d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
               </svg>
-              About
+              Profil
             </LinkScroll>
             <LinkScroll
               activeClass="active"
@@ -187,7 +187,7 @@ const Header = () => {
                   d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
                 />
               </svg>
-              Feature
+              Fitur
             </LinkScroll>
             <LinkScroll
               activeClass="active"
@@ -219,7 +219,7 @@ const Header = () => {
                   d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
               </svg>
-              Pricing
+              Layanan
             </LinkScroll>
             <LinkScroll
               activeClass="active"
