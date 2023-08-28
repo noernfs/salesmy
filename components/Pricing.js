b431f21593160748b0a4bbea98dd2006dba8fddf
Muhammad Aji <noer.nfs@gmail.com>
2023-08-29 06:32:20 +0700

    lilfix

--- components/Pricing.js
+++ components/Pricing.js
@@ -37,7 +37,7 @@ const Pricing = () => {
             <ScrollAnimationWrapper className="flex justify-center">
               <motion.div
                 variants={scrollAnimation}
-                className="flex flex-col justify-center items-center border-2 border-gray-500 rounded-xl py-4 px-12 lg:px-12 xl:px-20"
+                className="flex flex-col justify-center items-center border-2 border-gray-500 rounded-xl py-4 px-4 lg:px-12 xl:px-20"
                 whileHover={{
                   scale : 1.1,
                   transition: {
